53e10bbd58f649707e1bcb89dba85106cce451b8
Vitor Pires <79681907+vpsilvaa@users.noreply.github.com>
2025-01-31 19:58:36 +0000

    commit

--- app.py
+++ app.py
@@ -1,1 +1,72 @@
 import streamlit as st
+import gdown
+import tensorflow as tf
+import io
+from PIL import Image
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
+@st.cache_resource
+def carregar_modelo():
+    url = 'drive.google.com/file/d/15POGWa2CuGEDtn-piJlqT6gjlsIKxg5M/view?usp=drive_link'
+    gdown.download(url, 'model_quantizado16bits.tflile')
+    interpreter = tf.lite.Interpreter(model_path='model_quantizado16bits.tflile')
+    interpreter.allocate_tensors()
+
+    return interpreter
+
+def carregar_imagem():
+    upload_file=st.file_uploader('Arraste e solte uma imagem aqui ou clique para selecionar uma', type=['png','jpg','jpeg'])
+
+    if upload_file is not None:
+        image_data = upload_file.read()
+        image = Image.open(io.BytesIO(image_data))
+
+        st.image(image)
+        st.success('Imagem carregada com sucesso')
+
+        image = np.array(image, dtype=np.float32)
+        image = image / 255.0
+        image = np.expand_dims(image, axis=0)
+        return image
+
+def previsao(interpreter, image):
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index', image])
+    interpreter.invoke()
+    output_data = interpreter.get_tensor(output_details[0]['index'])
+
+    classes = ['BlackMeasles', 'LeafBlight', 'HealthyGrapes', 'BlackRot']
+
+    df = pd.DataFrame()
+    df['classes'] = classes
+    df['probabilidades (%)'] = 100 * output_data[0]
+
+    fig = px.bar(df, y='classes', x='probabilidades (%)',
+                 orientation='h', text='probabilidades (%)',
+                 title='Probabilidade de classes de doenças em uvas')
+    st.plotly_chart(fig)
+
+def main():
+    st.set_page_config(
+        page_title='Leaf Classification',
+		page_icon='🍇',
+    )
+    
+    st.write('Leaf Classification 🍇')
+	
+    #Carregar modelo
+    interpreter = carregar_modelo()
+
+    #Carregar image
+    image = carregar_imagem()
+
+    #Classifica
+    if image is not None:
+        previsao(interpreter,image)
+
+if __name__ == '__main__':
+    main()
